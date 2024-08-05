44f4c45353fb08a694e1e60ef8bca68e1b2e035d
Imajio <inkognito.gomel@gmail.com>
2024-08-05 23:40:43 +0200

    Fixed error

--- src/main/java/org/mrsecret/maniken/Maniken.java
+++ src/main/java/org/mrsecret/maniken/Maniken.java
@@ -3,33 +3,35 @@ package org.mrsecret.maniken;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
 public final class Maniken extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getLogger().info("I've been started");
-        Bukkit.getPluginManager().registerEvents(this, this);
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(this, this);
         createSteveHeadRecipe();
-        getCommand("setskin").setExecutor(new SetSkinCommand());
-    }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(ChatColor.BLUE + "Hello " + event.getPlayer().getName() + " !");
+        if (getCommand("setskin") != null) {
+            getCommand("setskin").setExecutor(new SetSkinCommand());
+        } else {
+            getLogger().severe("Command 'changehead' not found in plugin.yml");
+        }
     }
 
     private void createSteveHeadRecipe() {
         ItemStack steveHead = new ItemStack(Material.PLAYER_HEAD, 1);
         steveHead.getItemMeta().setDisplayName(ChatColor.GOLD + "Mannequin");
 
-        ShapedRecipe recipe = new ShapedRecipe(steveHead);
+        NamespacedKey key = new NamespacedKey(this, "steve_head");
+        ShapedRecipe recipe = new ShapedRecipe(key, steveHead);
         recipe.shape("LLL", "LHL", "LLL");
         recipe.setIngredient('L', Material.LEATHER);
         recipe.setIngredient('H', Material.OAK_PLANKS);
